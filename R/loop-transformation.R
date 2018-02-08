85cf88c61519c85c7bd4a9c091fdb5bd6d20c444
Thomas Mailund <mailund@birc.au.dk>
2018-02-08 09:14:34 +0100

    Documented internal functions
    We might as well document those functions as well...

--- R/loop-transformation.R
+++ R/loop-transformation.R
@@ -1,28 +1,39 @@
 
 ## Test for possibility of transformation #########################################
 
-# I need to import these for CHECK to work, so I might as well do it here...
+# I need to import the Depends packagefor CHECK to work, so I might as well do it here...
 # Other than satisfying CHECK, I'm not using these imports since I qualify the functions
 # by their namespace.
 
+#' Tests if a call object can be transformed.
+#'
+#' @param call_name Name (function) of the call.
+#' @param call_arguments The call's arguments
+#' @param fun_name The name of the recursive function we want to transform
+#' @param fun_call_allowed Whether a recursive call is allowed at this point
+#' @param cc Current continuation to abort if a transformation is not possible
+#'
+#' @return TRUE, if the expression can be transformed. Invokes \code{cc} otherwise.
+#'
 #' @import glue
 #' @import rlang
 can_call_be_transformed <- function(call_name, call_arguments,
                                     fun_name, fun_call_allowed, cc) {
     switch(call_name,
-           # Code blocks -- don't consider those calls.
-           "{" = {
-               for (arg in call_arguments) {
-                   can_transform_rec(arg, fun_name, fun_call_allowed, cc)
-               }
-           },
+        # Code blocks -- don't consider those calls.
+        "{" = {
+            for (arg in call_arguments) {
+                can_transform_rec(arg, fun_name, fun_call_allowed, cc)
+            }
+        },
 
-           # Selection
+        # Selection
         "if" = {
             can_transform_rec(call_arguments[[1]], fun_name, fun_call_allowed, cc)
             can_transform_rec(call_arguments[[2]], fun_name, TRUE, cc)
-            if (length(call_arguments) == 3)
-                can_transform_rec(call_arguments[[3]], fun_name, TRUE, cc)
+            if (length(call_arguments) == 3) {
+                  can_transform_rec(call_arguments[[3]], fun_name, TRUE, cc)
+              }
         },
 
         # Loops
@@ -58,6 +69,14 @@ can_call_be_transformed <- function(call_name, call_arguments,
     return(TRUE)
 }
 
+#' Recursive call for testing if an expression can be transformed into a looping tail-recursion.
+#'
+#' @param expr The expression to test
+#' @param fun_name The name of the recursive function we want to transform
+#' @param fun_call_allowed Whether a recursive call is allowed at this point
+#' @param cc Current continuation, used to escape if the expression cannot be transformed.
+#'
+#' @return TRUE, if the expression can be transformed. Invokes \code{cc} otherwise.
 can_transform_rec <- function(expr, fun_name, fun_call_allowed, cc) {
     if (rlang::is_atomic(expr) || rlang::is_pairlist(expr) ||
         rlang::is_symbol(expr) || rlang::is_primitive(expr)) {
@@ -74,7 +93,7 @@ can_transform_rec <- function(expr, fun_name, fun_call_allowed, cc) {
 #' Tests if a function, provided by its name, can be transformed.
 #'
 #' This function analyses a recursive function to check if we can transform it into
-#' a loop or trampoline version with \code{\link{transform}}. This version expects the
+#' a loop or trampoline version with \code{\link{loop_transform}}. This version expects the
 #' function to be provided as a quosure, but see also \code{\link{can_loop_transform}}.
 #'
 #' Since this function needs to handle recursive functions, it needs to know the name of
@@ -161,32 +180,34 @@ make_returns_explicit_call <- function(call_expr, in_function_parameter) {
     call_args <- rlang::call_args(call_expr)
 
     switch(call_name,
-           # For if-statments we need to treat the condition as in a call
-           # but the two branches will have the same context as the enclosing call.
-           "if" = {
-                call_expr[[2]] <- make_returns_explicit(call_args[[1]], TRUE)
-                call_expr[[3]] <- make_returns_explicit(call_args[[2]], in_function_parameter)
-                if (length(call_args) == 3)
-                    call_expr[[4]] <- make_returns_explicit(call_args[[3]], in_function_parameter)
-           },
+        # For if-statments we need to treat the condition as in a call
+        # but the two branches will have the same context as the enclosing call.
+        "if" = {
+            call_expr[[2]] <- make_returns_explicit(call_args[[1]], TRUE)
+            call_expr[[3]] <- make_returns_explicit(call_args[[2]], in_function_parameter)
+            if (length(call_args) == 3) {
+                  call_expr[[4]] <- make_returns_explicit(call_args[[3]], in_function_parameter)
+              }
+        },
 
-           # We don't treat blocks as calls and we only transform the last argument
-           # of the block. Explicit returns are the only way to exit the block in earlier
-           # statements, anyway
-           "{" = {
-               n <- length(call_expr)
-               call_expr[[n]] <- make_returns_explicit(call_expr[[n]], in_function_parameter)
-           },
+        # We don't treat blocks as calls and we only transform the last argument
+        # of the block. Explicit returns are the only way to exit the block in earlier
+        # statements, anyway
+        "{" = {
+            n <- length(call_expr)
+            call_expr[[n]] <- make_returns_explicit(call_expr[[n]], in_function_parameter)
+        },
 
-           # For all other calls we transform the arguments inside a call context.
-           {
-               for (i in seq_along(call_args)) {
-                   # call_expr[[i + 1]] <- make_returns_explicit(call_args[[i]], TRUE)
-               }
-               if (!in_function_parameter) # if we weren't parameters, we are a value to be returned
-                   call_expr <- rlang::call2("return", call_expr)
-           }
-           )
+        # For all other calls we transform the arguments inside a call context.
+        {
+            for (i in seq_along(call_args)) {
+                # call_expr[[i + 1]] <- make_returns_explicit(call_args[[i]], TRUE)
+            }
+            if (!in_function_parameter) { # if we weren't parameters, we are a value to be returned
+                  call_expr <- rlang::call2("return", call_expr)
+              }
+        }
+    )
 
     call_expr
 }
@@ -229,15 +250,19 @@ translate_recursive_call_into_next <- function(recursive_call, fun) {
     for (i in seq_along(assignments)) {
         new_vars[[i]] <- paste0("..", variables[[i]])
         new_assignments[[i]] <-
-            call("<-",
-                 rlang::sym(new_vars[[i]]),
-                 assignments[[i]])
+            call(
+                "<-",
+                rlang::sym(new_vars[[i]]),
+                assignments[[i]]
+            )
     }
     for (i in seq_along(assignments)) {
         assignments[[i]] <-
-            call("<-",
-                 rlang::sym(variables[[i]]),
-                 rlang::sym(new_vars[[i]]))
+            call(
+                "<-",
+                rlang::sym(variables[[i]]),
+                rlang::sym(new_vars[[i]])
+            )
     }
 
     as.call(c(rlang::sym("{"), new_assignments, assignments, `next`))
@@ -254,7 +279,6 @@ transform_recursive_calls <- function(expr, fun_name, fun) {
     if (rlang::is_atomic(expr) || rlang::is_pairlist(expr) ||
         rlang::is_symbol(expr) || rlang::is_primitive(expr)) {
         expr
-
     } else {
         stopifnot(rlang::is_lang(expr))
         call_name <- rlang::call_name(expr)
@@ -275,14 +299,16 @@ transform_recursive_calls <- function(expr, fun_name, fun) {
 }
 
 
-#' If a call is `{` and has a single expression inside it, replace it with that expression.
+#' Simplify nested code-blocks.
+#'
+#' If a call is \code{\{} and has a single expression inside it, replace it with that expression.
+#'
 #' @param expr The expression to rewrite
 #' @return The new expression
 simplify_nested_blocks <- function(expr) {
     if (rlang::is_atomic(expr) || rlang::is_pairlist(expr) ||
         rlang::is_symbol(expr) || rlang::is_primitive(expr)) {
         expr
-
     } else {
         stopifnot(rlang::is_lang(expr))
         call_name <- rlang::call_name(expr)
@@ -298,6 +324,16 @@ simplify_nested_blocks <- function(expr) {
     }
 }
 
+#' Construct the expression for a transformed function body.
+#'
+#' This is where the loop-transformation is done. This function translates
+#' the body of a recursive function into a looping function.
+#'
+#' @param fun_expr The original function body.
+#' @param fun_name The name of the recursive function.
+#' @param fun The actual function definition.
+#'
+#' @return The body of the transformed function.
 build_transformed_function <- function(fun_expr, fun_name, fun) {
     fun_expr <- make_returns_explicit(fun_expr, FALSE)
     fun_expr <- transform_recursive_calls(fun_expr, fun_name, fun)
