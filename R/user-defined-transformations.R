079392d86cdad870f72830bd5a749074f353ff48
Thomas Mailund <mailund@birc.au.dk>
2018-02-28 11:58:10 +0100

    Got rid of dead code
    With the new handling of user-defined transformation, I don't need the generic functions.

--- R/user-defined-transformations.R
+++ R/user-defined-transformations.R
@@ -1,47 +1,4 @@
 
-#' Transform a call before the tail-recursion transformation.
-#'
-#' This generic function is a hook by which you can modify how the
-#' tail-recursion transformations should handle special functions.
-#' It gives you a way to rewrite function calls to make them tail-recursive
-#' before we do any other manipulation.
-#'
-#' @param fun   The actual function. Used for dynamic dispatching.
-#' @param expr  The expression to rewrite.
-#' @return The rewritten expression.
-#'
-#' @examples
-#' my_if_else <- function(test, if_true, if_false) {
-#'     if (test) if_true else if_false
-#' }
-#' f <- function(x, y) my_if_else(x == y, x, f(y, y))
-#' f(1, 2)
-#' f(3, 3)
-#'
-#' can_loop_transform(f) # No, we can't, and we get a warning
-#'
-#' class(my_if_else) <- c("my_if_else", class(my_if_else))
-#' class(my_if_else)
-#'
-#' transform_call.my_if_else <- function(fun, expr) {
-#'     test <- expr[[2]]; if_true <- expr[[3]]; if_false <- expr[[4]]
-#'     rlang::expr(if (rlang::UQ(test)) rlang::UQ(if_true) else rlang::UQ(if_false))
-#' }
-#' transform_call(my_if_else, quote(my_if_else(x == y, x, f(y, y))))
-#'
-#' can_loop_transform(f) # Now we can, because my_if_else gets transformed
-#'
-#' @export
-transform_call <- function(fun, expr) {
-    UseMethod("transform_call")
-}
-
-#' @describeIn transform_call The default is to just return the unchanged expression.
-#' @export
-transform_call.default <- function(fun, expr) {
-    expr
-}
-
 #' Apply user transformations depths-first.
 #'
 #' The difference between this function and \code{\link{user_transform}} is that the this
